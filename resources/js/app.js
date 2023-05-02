0c7fae65a8bbbd0e586c41ea256ce42820feb4d1
broishungry <broishungry@gmail.com>
2023-05-02 17:53:26 +0300

    try to add vueJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+//import App from "./App";            // х\з куда его
+//import App from "./components/App"  // или это
+
+// Вроде так как то должно быть но npm ругается на эту строку
+app.component('app', require('./components/App'))//.default) // не робит
+const app = new Vue({
+    el: '#app'
+})//.mount('#app') //не уверен что это нужно
+
+
+
+/* // не робит
+ import { createApp } from 'vue'
+ import App from './App.vue'
+ createApp(App).mount('#app')
+ */
+
